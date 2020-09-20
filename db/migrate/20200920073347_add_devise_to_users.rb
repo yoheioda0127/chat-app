7c7766989b5724194215b5b4c3a6a42ef6e8ab08
yoheioda0127 <maikemi00romans@gamil.com>
2020-09-20 17:26:00 +0900

    deviseに関するビューファイルの配置

--- db/migrate/20200920073347_add_devise_to_users.rb
+++ db/migrate/20200920073347_add_devise_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+class AddDeviseToUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
