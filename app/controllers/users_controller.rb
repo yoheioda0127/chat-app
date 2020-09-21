9aeaf7e487fa56a11f20125f4fee64f313d17f55
yoheioda0127 <maikemi00romans@gamil.com>
2020-09-21 10:58:42 +0900

    ユーザー編集機能の実装・ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,12 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
-end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
