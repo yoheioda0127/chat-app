ee1d9c2fbc831edc6666911620eb3d7bf5e34e43
yoheioda0127 <maikemi00romans@gamil.com>
2020-09-22 07:48:56 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_many :rooms_users
+  has_many :users, through: :room_users
 end
---
+++ db/migrate/20200921222031_create_room_users.rb
@@ -0,0 +1,11 @@
+class CreateRoomUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :room_users do |t|
+      t.references :room, foreign_key: true
+      #外部キー
+      t.references :user, foreign_key: true
+      #外部キー
+      t.timestamps
+    end
+  end
+end
